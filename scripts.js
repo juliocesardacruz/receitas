d7e1ee4fca1f74e915bcbda4fb50ef804f7a03e6
Julio Cesar <jucecr@hotmail.com>
2025-11-21 00:28:01 -0300

    a

---
+++ scripts.js
@@ -0,0 +1,19 @@
+const receitas = [
+    {
+        id: 1,
+        title: "Arroz à Grega",
+        link: a,
+        image: "imagens/arroz à grega.jpeg"
+    },
+    {
+        id:2,
+        title: "imagens/Bolinho de arroz.jpeg",
+        link:a ,
+        image: ""
+    }
+    
+];
+
+document.addEventListener('DOMContentLoaded', () => {
+    alert("Carregou a página")
+})
